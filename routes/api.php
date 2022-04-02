be0ea2fd75d606cf965613c94805ddd3c8fd89aa
zehanfi <102772032+zehanfi@users.noreply.github.com>
2022-04-02 19:59:20 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,32 +17,23 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/place/details', function(){
+Route::get('/place/details', [\App\Http\Controllers\WrapperApiController::class, 'details'])->name('details');
+Route::get('/place/findplacefromtext', [\App\Http\Controllers\WrapperApiController::class, 'findplacefromtext'])->name('findplacefromtext');
+Route::get('/place/nearbysearch', [\App\Http\Controllers\WrapperApiController::class, 'nearbysearch'])->name('nearbysearch');
+Route::get('/place/textsearch', [\App\Http\Controllers\WrapperApiController::class, 'textsearch'])
+    ->middleware( \App\Http\Middleware\NpmMiddleware::class)
+    ->name('textsearch');
+Route::get('/place/photo', [\App\Http\Controllers\WrapperApiController::class, 'photo'])->name('photo');
+Route::get('/user/identitas', function(){
     return[
         'code' => '0',
         'data' => [
             'npm' => '197006073',
             'nama' => 'Zhehan Gustiyandi',
             'email' => 'zehan@gmail.com',
-            'alamat' => 'Kawalu tasikmalaya',
-            'no hp' => '085218372010'
+            'AlamatLengkap' => 'Jl air tanjung no 25, kec kawalu'
         ]
         ];
-})->name('details');
-Route::get('/place/findplacefromtext', [\App\Http\Controllers\WrapperApiController::class, 'findplacefromtext'])->name('findplacefromtext');
-Route::get('/place/nearbysearch', [\App\Http\Controllers\WrapperApiController::class, 'nearbysearch'])->name('nearbysearch');
-Route::get('/place/textsearch', [\App\Http\Controllers\WrapperApiController::class, 'textsearch'])
-    ->middleware( \App\Http\Middleware\NpmMiddleware::class)
-    ->name('textsearch');
-Route::get('/place/photo', function(){
-    return[
-        'code' => '0',
-        'data' => [
-            'npm' => '197006073',
-            'nama' => 'Zhehan Gustiyandi',
-            'email' => 'zehan@gmail.com'
-        ]
-        ];
-})->name('photo');
+})->name('identitas');
 
 
